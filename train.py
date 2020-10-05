0cce901af21f6a12081786447559da404651b3e8
vinoth654321 <56352860+vinoth654321@users.noreply.github.com>
2020-10-05 21:31:58 +0900

    Create train.py

---
+++ train.py
@@ -0,0 +1,54 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Open the image
+img = np.array(Image.open('flower-1.png')).astype(np.uint8)
+
+# Apply gray scale
+gray_img = np.round(0.299 * img[:, :, 0] +
+                    0.587 * img[:, :, 1] +
+                    0.114 * img[:, :, 2]).astype(np.uint8)
+
+# Prewitt Operator
+h, w = gray_img.shape
+# define filters
+horizontal = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])  # s2
+vertical = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])  # s1
+
+# define images with 0s
+newgradientImage = np.zeros((h, w))
+
+# offset by 1
+for i in range(1, h - 1):
+    for j in range(1, w - 1):
+        horizontalGrad = (horizontal[0, 0] * gray_img[i - 1, j - 1]) + \
+                         (horizontal[0, 1] * gray_img[i - 1, j]) + \
+                         (horizontal[0, 2] * gray_img[i - 1, j + 1]) + \
+                         (horizontal[1, 0] * gray_img[i, j - 1]) + \
+                         (horizontal[1, 1] * gray_img[i, j]) + \
+                         (horizontal[1, 2] * gray_img[i, j + 1]) + \
+                         (horizontal[2, 0] * gray_img[i + 1, j - 1]) + \
+                         (horizontal[2, 1] * gray_img[i + 1, j]) + \
+                         (horizontal[2, 2] * gray_img[i + 1, j + 1])
+
+        verticalGrad = (vertical[0, 0] * gray_img[i - 1, j - 1]) + \
+                       (vertical[0, 1] * gray_img[i - 1, j]) + \
+                       (vertical[0, 2] * gray_img[i - 1, j + 1]) + \
+                       (vertical[1, 0] * gray_img[i, j - 1]) + \
+                       (vertical[1, 1] * gray_img[i, j]) + \
+                       (vertical[1, 2] * gray_img[i, j + 1]) + \
+                       (vertical[2, 0] * gray_img[i + 1, j - 1]) + \
+                       (vertical[2, 1] * gray_img[i + 1, j]) + \
+                       (vertical[2, 2] * gray_img[i + 1, j + 1])
+
+        # Edge Magnitude
+        mag = np.sqrt(pow(horizontalGrad, 2.0) + pow(verticalGrad, 2.0))
+        newgradientImage[i - 1, j - 1] = mag
+
+
+plt.figure()
+plt.title('flower-1.png')
+plt.imsave('flower-1.png', newgradientImage, cmap='gray', format='png')
+plt.imshow(newgradientImage, cmap='gray')
+plt.show()
